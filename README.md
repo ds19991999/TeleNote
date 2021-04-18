3fa060e123f187c2db50d0045d0a12dadb2f93f3
mensondzh <cva.engineer.ding@gmail.com>
2021-04-18 11:42:11 +0800

    init

--- README.md
+++ README.md
@@ -1,38 +1,9 @@
-# TeleNote
+## 创建账号
 
-[![GitHub issues](https://img.shields.io/github/issues/ds19991999/TeleNote.svg)](https://github.com/ds19991999/TeleNote/issues)
-[![DUB](https://img.shields.io/dub/l/vibe-d.svg)](https://github.com/ds19991999/TeleNote/blob/master/LICENSE)
-[![GitHub release](https://img.shields.io/github/release/ds19991999/TeleNote.svg)](https://github.com/ds19991999/TeleNote/releases)
-[![Badge](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu/#/zh_CN)
-[![GitHub stars](https://img.shields.io/github/stars/ds19991999/TeleNote.svg?style=popout&label=Stars)](https://github.com/ds19991999/TeleNote/stargazers)
-[![GitHub forks](https://img.shields.io/github/forks/ds19991999/TeleNote.svg?style=popout&label=Fork)](https://github.com/ds19991999/TeleNote/fork)
-
-[`telenote`](https://github.com/ds19991999/TeleNote) 使用 [`telegraph`](https://telegra.ph) 构建。[`telegraph`](https://telegra.ph) 官网无法编辑已发布的文章，并且没有提供账户管理，文章管理，于是该脚本应用诞生，通过使用唯一的 `access_token`创建文章，使用 `auth_url` 获取历史文章的编辑权限。目前，该应用还未正式开发，未来将构建客户端笔记应用，先留个坑吧 。。。
-
-## 环境
+* https://api.telegra.ph/createAccount?short_name=Sandbox&author_name=Anonymous
 
 
-```shell
-git clone https://github.com/ds19991999/TeleNote.git
-python3 -m pip install requests[socks]==2.22.0
-```
-
-## 配置
-
-新建 `config.yaml` 文件，填写相关参数。
-
-## 运行
-
-`cmd` 直接运行即可，代理需要配置正确，否则会抛出 `requests.exceptions.ConnectionError` 异常。
-
-![image-20191204195252105](assets/image-20191204195252105.png)
-
-![image-20191204195327778](assets/image-20191204195327778.png)
-
-![image-20191204195350787](assets/image-20191204195350787.png)
+pyinstaller -F --icon=telenote.png telenote.py
 
 
 
-## License
-
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
---
+++ telegraph/api.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     utils.py
+   Description : https://telegra.ph/api
+   Author :        admin
+   date：          2019/12/4
+-------------------------------------------------
+   Change Activity:
+                   2019/12/4
+-------------------------------------------------
+"""
+
+__author__ = 'admin'
+
+
+import json
+import requests
+from requests.exceptions import ConnectionError
+
+
+class TelegraphException(Exception):
+    pass
+
+
+class TelegraphAPI(object):
+    __slots__ = ('access_token', 'session', 'headers', 'reconnect_times', 'my_proxies', 'timeout')
+    def __init__(self, access_token=None, reconnect_times=3, my_proxies=None, timeout=5):
+        self.access_token = access_token
+        self.timeout = timeout
+        self.reconnect_times = reconnect_times
+        self.my_proxies = my_proxies
+        self.session = requests.session()
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"}
+
+    def method(self, mehtod, values=None, path=""):
+        values = values if values is not None else {}
+        if "access_token" not in values and self.access_token:
+            values['access_token'] = self.access_token
+        req_url = "https://api.telegra.ph/{}/{}".format(mehtod, path)
+        try:
+            response = self.session.post(url=req_url, data=values, headers = self.headers,
+                                         proxies=self.my_proxies, timeout=self.timeout).json()
+            connected = True
+        except ConnectionError:
+            connected = False
+        for reconnect_time in range(self.reconnect_times):
+            if not connected:
+                try:
+                    response = self.session.post(url=req_url, data=values, headers = self.headers,
+                                                 proxies=self.my_proxies, timeout=self.timeout).json()
+                    connected = True
+                except ConnectionError:
+                    pass
+            else:
+                break
+        if connected:
+            try:
+                return response["result"]
+            except Exception:
+                print(response)
+        else:
+            raise ConnectionError
+
+
+class Telegraph(object):
+    __slots__ = ("_telegraph")
+
+    def __init__(self, access_token=None, reconnect_times=3, my_proxies=None, timeout=5):
+        self._telegraph = TelegraphAPI(access_token, reconnect_times, my_proxies, timeout)
+
+    def get_access_token(self):
+        return self._telegraph.access_token
+
+    def create_account(self,short_name, author_name=None,
+                       author_url=None,replace_token=True):
+        """
+
+        :param short_name:
+        :param author_name:
+        :param author_url:
+        :param replace_token:
+        :return:dict
+        """
+        method = "createAccount"
+        values = {
+            'short_name': short_name,
+            'author_name': author_name,
+            'author_url': author_url
+        }
+        response = self._telegraph.method(method,values)
+        if replace_token:
+            self._telegraph.access_token = response.get("access_token")
+        return response
+
+    def edit_account_info(self, short_name=None, author_name=None, author_url=None):
+        """
+
+        :param short_name:
+        :param author_name:
+        :param author_url:
+        :return:
+        """
+        method = "editAccountInfo"
+        values = {
+            'short_name': short_name,
+            'author_name': author_name,
+            'author_url': author_url
+        }
+        response = self._telegraph.method(method, values)
+        return response
+
+    def revoke_access_token(self):
+        """
+
+        :return:
+        """
+        method = "revokeAccessToken"
+        values = None
+        response = self._telegraph.method(method, values)
+        self._telegraph.access_token = response.get('access_token')
+        return response
+
+    def get_page(self, path, return_content=True):
+        """
+
+        :param path:
+        :param return_content:
+        :param return_html:
+        :return:
+        """
+        method = "getPage"
+        values = {
+            'return_content': return_content
+        }
+        response = self._telegraph.method(method, values, path)
+        return response
+
+    def create_page(self, title, content=None, author_name=None,
+                    author_url=None, return_content=True):
+        """
+
+        :param title:
+        :param content:
+        :param html_content:
+        :param author_name:
+        :param author_url:
+        :param return_content:
+        :return:
+        """
+        content_json = json.dumps(content)
+        method = "createPage"
+        values = {
+            'title': title,
+            'author_name': author_name,
+            'author_url': author_url,
+            'content': content_json,
+            'return_content': return_content
+        }
+        response = self._telegraph.method(method, values)
+        return response
+
+    def edit_page(self, path, title, content=None, author_name=None,
+                  author_url=None, return_content=False):
+        """
+
+        :param path:
+        :param title:
+        :param content:
+        :param author_name:
+        :param author_url:
+        :param return_content:
+        :return:
+        """
+        content_json = json.dumps(content)
+        method = "editPage"
+        values = {
+            'title': title,
+            'author_name': author_name,
+            'author_url': author_url,
+            'content': content_json,
+            'return_content': return_content
+        }
+        response = self._telegraph.method(method, values, path)
+        return response
+
+    def get_account_info(self, fields=None):
+        """
+
+        :param fields:
+        :return:
+        """
+        method = "getAccountInfo"
+        values = {
+            'fields': json.dumps(fields) if fields else None
+        }
+        response = self._telegraph.method(method, values)
+        return response
+
+    def get_page_list(self, offset=0, limit=50):
+        """
+
+        :param offset:
+        :param limit:
+        :return:
+        """
+        method = "getPageList"
+        values = {
+            'offset': offset,
+            'limit': limit
+        }
+        response = self._telegraph.method(method, values)
+        return response
+
+    def get_views(self, path, year=None, month=None, day=None, hour=None):
+        """
+
+        :param path:
+        :param year:
+        :param month:
+        :param day:
+        :param hour:
+        :return:
+        """
+        method = "getViews"
+        values = {
+            'year': year,
+            'month': month,
+            'day': day,
+            'hour': hour
+        }
+        response = self._telegraph.method(method, values)
+        return response
+
+if __name__ == "__main__":
+    access_token = "d0f2bea3fd70a5d4a0b59b310f6d75c135a0bf5fbab8354867523cf4b11f"
+    author_name = "anonymous"
+    author_url = "https://t.me/baba2333"
+    short_name = "ds19991999"
+    my_proxies = {
+        "http": "socks5://127.0.0.1:1080",
+        "https": "socks5://127.0.0.1:1080"
+    }
+    telegraph = Telegraph(access_token=access_token, my_proxies=my_proxies)
+    content = [{"tag":"p","children":["Hello,+world!"]}]
+    a = telegraph.create_page(title="2sddedec23d2", content=content, author_name=author_name, author_url=author_url, return_content=True)
+    print(a)
